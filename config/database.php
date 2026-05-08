51cb783a9afc3721ee3fef386c177c38d6c76f6f
saul9809 <slionking09@gmail.com>
2026-05-08 12:33:53 -0400

    Recuoerando proyecto dañado

--- config/database.php
+++ config/database.php
@@ -90,7 +90,7 @@ return [
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'ia_sdk_test'),
             'username' => env('DB_USERNAME', 'postgres'),
-            'password' => env('DB_PASSWORD', ''),
+            'password' => env('DB_PASSWORD', 'root'),
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
